4ae7ab568bebc736b6986dc125f5795cada69371
Sky53 <Sanyaga3@yandex.ru>
2019-01-17 13:45:38 +0300

    Fix bug for reload update method, add local jquery

--- src/main/resources/templates/js/eventUpdate.js
+++ src/main/resources/templates/js/eventUpdate.js
@@ -1,4 +1,5 @@
-$(function () {
+$(document).ready(function () {
+
     $(".updateCom").on("click", function () {
 
         let a = $(this).parents(".component-row").first();
@@ -6,20 +7,16 @@ $(function () {
         let name = a.find(".name").first().text();
         let need = a.find(".need").first().text() === "да";
         let count = a.find(".count").first().text();
-                      console.log(id, name, need, count);
+        console.log(id, name, need, count);
         document.getElementById('idInputForm').value = id;
         document.getElementById('idNameForm').value = name;
         document.getElementById('idNeedForm').value = need;
         document.getElementById('idCountForm').value = count;
-        //     $.post("/updates", {id, name, need, count});
-     //   alert("Передо во всплывающее");
+  
         return false;
-
     });
 
-});
 
-$(function () {
     $(".updateButton").on("click", function () {
 
         let id = document.getElementById("idInputForm").value;
@@ -27,14 +24,16 @@ $(function () {
         let need = document.getElementById("idNeedForm").value;
         let count = document.getElementById("idCountForm").value;
         console.log(id, name, need, count);
-      //  alert("Передано на сервер");
-        $.post("/updates", {id, name, need, count});
 
-   //     alert("Перезагрузка");
-        setTimeout(function() {window.location.reload();}, 1);
+        $.post("/updates", {id, name, need, count}, function () {
+            location.reload();
+        });
+
+
         return false;
 
 
     });
+
 
 });
